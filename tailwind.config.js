6646c1b11e352f01854d7e530a51ac74a8c82679
Muhammad Sayuti <sayutizxc@gmail.com>
2021-10-11 21:47:59 +0800

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '10xl': '10rem',
+      },
+      screens: {
+        xs: '540px',
+      },
+      fontFamily: {
+        scheherazade: "'Scheherazade New', serif",
+      },
+      animation: {
+        scale: 'scale 3s infinite',
+        'pulse-slow': 'pulse 2s ease-in-out infinite',
+      },
+      keyframes: {
+        scale: {
+          '0%, 100%': {
+            transform: 'scale(1) rotate(7deg)',
+          },
+          '50%': {
+            transform: 'scale(1.2) rotate(-7deg)',
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
