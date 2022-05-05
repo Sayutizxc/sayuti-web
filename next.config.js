235e51cb83900f2eef36ef6177db49b533b79618
Muhammad Sayuti <sayutizxc@gmail.com>
2022-05-05 17:50:23 +0800

    redirect url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/wa",
+        destination: "https://wa.me/%2B18088009191",
+        permanent: true,
+      },
+      {
+        source: "/tele",
+        destination: "https://t.me/sayutizxc",
+        permanent: true,
+      },
+    ];
+  },
 };
